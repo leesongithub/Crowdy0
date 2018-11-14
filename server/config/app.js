ae27d69f475929477998b2c841b072533e5241c1
Leeson Chen <32338309+leesongithub@users.noreply.github.com>
2018-11-13 20:35:32 -0500

    no longer 3000

--- server/config/app.js
+++ server/config/app.js
@@ -4,7 +4,7 @@ var config = require('./config'),
 
 module.exports.start = function() {
   var app = express.init();
-  app.listen(3000, function() {
+  app.listen(config.port, function() {
     console.log('App listening on port', config.port);
   });
 };
